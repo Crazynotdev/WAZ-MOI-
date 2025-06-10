92343923a4bd0d418dd2ee4adff329bd3ea26606
DavidTechInc <techigwe@gmail.com>
2025-06-10 20:23:10 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function generateLink() {
+  const username = document.getElementById("username").value.trim().toLowerCase();
+  if (username) {
+    window.location.href = `u/${username}.html`;
+  } else {
+    alert("Entre un pseudo !");
+  }
+}
+
+function sendMessage(username) {
+  const message = document.getElementById("message").value.trim();
+  if (message.length === 0) return alert("Écris quelque chose !");
+  const key = `inbox_${username}`;
+  const inbox = JSON.parse(localStorage.getItem(key) || "[]");
+  inbox.push(message);
+  localStorage.setItem(key, JSON.stringify(inbox));
+  alert("Message envoyé anonymement !");
+  document.getElementById("message").value = "";
+}
